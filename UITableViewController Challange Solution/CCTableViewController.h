33c6b6c4706e3bb5785d49993a5c10f093d1b8da
tmudunge <tendaimudunge@Tendais-MacBook-Pro.local>
2014-08-05 09:48:58 -0600

    solved the challenge

---
+++ UITableViewController Challange Solution/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewController Challange Solution
+//
+//  Created by Tendai Mudunge on 2014-08-05.
+//  Copyright (c) 2014 ___Tendai Mudunge___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
